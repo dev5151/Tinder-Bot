70e0b057651b76e13592525ced1c6622f054c2a1
dev5151 <iamdev5151@gmail.com>
2020-04-18 15:00:13 +0530

    driver setup

--- tinderBot.py
+++ tinderBot.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
 
-driver = webdriver.Chrome(executable_path=r'C:\webdrivers')
+driver=webdriver.Chrome(executable_path=r'C:\webdrivers\chromedriver.exe')
 
 class TinderBot():
     def _init_(self):
-        self.driver=driver
+        self.driver=webdriver.Chrome()
 
+    def login(self):
+        driver.get('https://tinder.com')
