5c06ac34abe42849bf6201a1ea38e18ee8dfbfef
dev5151 <iamdev5151@gmail.com>
2020-04-18 17:39:41 +0530

    Added Facebook Button Selector

--- tinderBot.py
+++ tinderBot.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+import time
 
 driver=webdriver.Chrome(executable_path=r'C:\webdrivers\chromedriver.exe')
 
@@ -8,3 +9,9 @@ class TinderBot():
 
     def login(self):
         driver.get('https://tinder.com')
+        time.sleep(10)
+        more_options_btn=driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/span/button')
+        more_options_btn.click()
+        time.sleep(10)
+        fb_btn=driver.find_element_by_xpath('//span[text()="Log in with Facebook"]')
+        fb_btn.click()
